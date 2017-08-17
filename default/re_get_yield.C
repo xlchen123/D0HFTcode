ec82535389934e6ca197178a491df9f4650b54fa
Guannan Xie <guannanxie@lbl.gov>
2017-08-17 16:36:10 -0500

    update re_getyield.C

--- default/re_get_yield.C
+++ default/re_get_yield.C
@@ -40,15 +40,25 @@ void re_get_yield()  //re calculate the default mean value, especially for some 
       {
         y[ipt] = y1[ipt];
         yerr[ipt] = y1err[ipt];
-        if(icent == 1 && ipt == 0)  //10-20% 0-0.5GeV data point
+        if( icent == 1 && ipt == 0)  //10-20%, 0-0.5GeV data point // average all the data point
         {
           y[ipt] = 1./4 * (y1[ipt] + y2[ipt] + y3[ipt] + y4[ipt]);
           yerr[ipt] = y[ipt] * 1./4. * (y1err[ipt]*1./y1[ipt] + y2err[ipt]*1./y2[ipt]+ y3err[ipt]*1./y3[ipt] + y4err[ipt]*1./y4[ipt]);
         }
-        if(icent == 4 && (ipt >= npt -3) )  //60-80% 4-5-6-8GeV data point
+        if( icent ==0  && ipt == 0)  //0-10%, 0-0.5GeV data point // average 3 of the data points, sicne the last signal from likesign is less then 3sigma
         {
-          y[ipt] = 1./4 * (y1[ipt] + y2[ipt] + y3[ipt] + y4[ipt]);
-          yerr[ipt] = y[ipt] * 1./4. * (y1err[ipt]*1./y1[ipt] + y2err[ipt]*1./y2[ipt]+ y3err[ipt]*1./y3[ipt] + y4err[ipt]*1./y4[ipt]);
+          y[ipt] = 1./3 * (y1[ipt] + y2[ipt] + y3[ipt] );
+          yerr[ipt] = y[ipt] * 1./3. * (y1err[ipt]*1./y1[ipt] + y2err[ipt]*1./y2[ipt]+ y3err[ipt]*1./y3[ipt]);
+        }
+        if(icent == 4 && ipt > npt -3 )  //60-80% 5-6-8GeV data point// 
+        {
+          y[ipt] = 1./3 * (y1[ipt] + y2[ipt] +  y4[ipt]);
+          yerr[ipt] = y[ipt] * 1./3. * (y1err[ipt]*1./y1[ipt] + y2err[ipt]*1./y2[ipt]+ y4err[ipt]*1./y4[ipt]);
+        }
+        if(icent == 4 && ipt == npt -3 )  //60-80% 4-5GeV data point// 
+        {
+          y[ipt] = y2[ipt];// fit didn't catch the raw counts
+          yerr[ipt] = y2err[ipt]*1.;
         }
       }
 
